985cb9ffba96ed169183cd54c4c2dbe6746073a4
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-28 03:02:07 +0000

    Moved the express CRUD operatiosn from the 'server.js' into a new routes file 'musician.js' found in the sub folder 'routes'. Set the end point for the Musician model, and required in the 'musician.js' which contains the express router path for the Musican model CRUD operations.

--- server.js
+++ server.js
@@ -1,53 +1,10 @@
 const express = require("express");
 const app = express();
-const {Musician} = require("./models/Musician")
+const musicianRouter = require("./routes/musician")
 const {sequelize} = require("./db")
-
 const port = 3000;
 
-//TODO
-// app.use(express.static("public"))
-/* app.use(express.json())
-
-app.post("/musicians", async function(request, response) {
-    try{
-        const musician = await Musician.create(request.body)
-        response.status(200).send(Musician.findAll())
-    }
-    catch(error){
-        response.send(500).status({error: error.message})
-    }
-})
-
-app.get("/musicians/:id", async function(request, response) {
-    try{
-        const musician = await Musician.findByPk(request.params.id)
-        response.status(200).send(musician)
-    }
-    catch(error){
-        response.status(500).send({error: error.message})
-    }
-})
-
-app.put("/musicians/:id", async function(request, response) {
-    try{
-        const musician = await Musician.update(request.body, {where: {id: request.params.id}})
-        response.status(200).send(musician)
-    }
-    catch(error){
-        response.status(500).send({error: error.message})
-    }
-})
-
-app.delete("/musicians/:id", async function(request, response) {
-    try{
-        const musician = await Musician.destroy({where: {id: request.params.id}})
-        response.status(200).send({musician})
-    }
-    catch(error){
-        response.status(500).send({error: error.message})
-    }
-}) */
+app.use("/musicians", musicianRouter)
 
 app.listen(port, () => {
     sequelize.sync();
