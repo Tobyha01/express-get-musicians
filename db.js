8f382c337cd8004faf3c9d3572600d4324fe0d08
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-28 03:21:12 +0000

    Changed the import of 'Model' to 'DataTypes', made sure to also export it for use in other folderds.

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { Sequelize, Model } = require('sequelize');
+const {Sequelize, DataTypes} = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
@@ -9,5 +9,5 @@ const sequelize = new Sequelize({
 
 module.exports = {
     sequelize,
-    Sequelize
+    DataTypes
 };
